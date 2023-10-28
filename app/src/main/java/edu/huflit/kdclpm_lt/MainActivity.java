65af430febc6f57599995956c8cff7d86a5dc2d4
phamdinhkhoi <paradise07102002@gmail.com>
2023-10-29 02:09:50 +0700

    Tạo sẵn tài khoản admin (Khi ở màn hình giới thiệu

--- app/src/main/java/edu/huflit/kdclpm_lt/MainActivity.java
+++ app/src/main/java/edu/huflit/kdclpm_lt/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import edu.huflit.kdclpm_lt.Object.User;
+import edu.huflit.kdclpm_lt.SQLite.MyDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
+    MyDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Tạo tài khoản admin
+        boolean checkAdmin  = database.checkAdmin();
+        if (checkAdmin == false) {
+            User user = new User();
+            //Dùng set để gán dữ liệu cho biến sach
+            user.setUsername_user("admin");
+            user.setPassword_user("admin");
+            user.setFullname_user("Phạm Đình Khôi");
+            user.setEmail_user("admin@gmail.com");
+            user.setPhone_user("0123456789");
+            user.setRole_user("admin");
+            user.setLoai_kh_user("vip3");
+            //Trả về loaisach chứa các thông tin của loại sách
+            database.addUser(user);
+        }
     }
 }
