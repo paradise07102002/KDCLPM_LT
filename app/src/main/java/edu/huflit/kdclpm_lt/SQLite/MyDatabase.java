c7294f1f88684c5c5ff6840d1ac4e638d2f8a306
phamdinhkhoi <paradise07102002@gmail.com>
2023-10-29 01:58:57 +0700

    Tạo MyDatabase

--- app/src/main/java/edu/huflit/kdclpm_lt/SQLite/MyDatabase.java
+++ app/src/main/java/edu/huflit/kdclpm_lt/SQLite/MyDatabase.java
@@ -1,2 +1,15 @@
-package edu.huflit.kdclpm_lt.SQLite;public class MyDatabase {
+package edu.huflit.kdclpm_lt.SQLite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class MyDatabase {
+    SQLiteDatabase database;
+    DBHelper helper;
+
+    public MyDatabase(Context context)
+    {
+        helper = new DBHelper(context);
+        database = helper.getWritableDatabase();
+    }
 }
