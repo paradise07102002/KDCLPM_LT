d4a86b92850e46e512dbae7ea88d24d292f48fff
phamdinhkhoi <paradise07102002@gmail.com>
2023-10-29 02:08:46 +0700

    Hàm thêm User

--- app/src/main/java/edu/huflit/kdclpm_lt/SQLite/MyDatabase.java
+++ app/src/main/java/edu/huflit/kdclpm_lt/SQLite/MyDatabase.java
@@ -1,8 +1,11 @@
 package edu.huflit.kdclpm_lt.SQLite;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import edu.huflit.kdclpm_lt.Object.User;
 
 public class MyDatabase {
     SQLiteDatabase database;
@@ -25,5 +28,17 @@ public class MyDatabase {
         }
         cursor.close();
         return false;
+    }
+    public long addUser(User user)
+    {
+        ContentValues values = new ContentValues();
+        values.put(DBHelper.USERNAME_USER, user.getUsername_user());
+        values.put(DBHelper.PASSWORD_USER, user.getPassword_user());
+        values.put(DBHelper.FULLNAME_USER, user.getFullname_user());
+        values.put(DBHelper.EMAIL_USER, user.getEmail_user());
+        values.put(DBHelper.PHONE_USER, user.getPhone_user());
+        values.put(DBHelper.ROLE_USER, user.getRole_user());
+        values.put(DBHelper.LOAI_KH_USER, user.getLoai_kh_user());
+        return database.insert(DBHelper.TABLE_USER, null, values);
     }
 }
